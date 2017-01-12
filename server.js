d31ee7c2b64544edda62edd8a29fc099afd6fc11
meg4r0m <psykoterro@gmail.com>
2017-01-12 11:02:35 +0100

    Livre d'Or complet

--- server.js
+++ server.js
@@ -16,21 +16,34 @@ app.use(session({
     saveUninitialized: true,
     cookie: { secure: false }
 }))
+app.use(require('./middlewares/flash'))
 
 // Routes
 app.get('/', (request, response) => {
-    if (request.session.error){
-        response.locals.error = request.session.error
-        request.session.error = undefined
-    }
-    response.render('pages/index')
+    let Message = require('./models/message')
+    Message.all(function (messages) {
+        response.render('pages/index', {messages: messages})
+    })
 })
 
 app.post('/', (request, response) => {
-    if (request.body.message === undefined || request.body.message === ''){
-        request.session.error = "Il y a une erreur"
+    if (request.body.message === undefined || request.body.message === '') {
+        request.flash('error', "Vous n'avez pas posté de message")
         response.redirect('/')
+    } else {
+        let Message = require('./models/message')
+        Message.create(request.body.message, function () {
+            request.flash('success', "Merci !")
+            response.redirect('/')
+        })
     }
+})
+
+app.get('/message/:id', (request, response) => {
+    let Message = require('./models/message')
+    Message.find(request.params.id, function (message) {
+        response.render('messages/show', {message: message})
+    })
 })
 
 app.listen(8080)
